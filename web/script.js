03b56715f0696aee9b9a00d3e9efe4a7accf7fab
Karl Landström <karl.landstrom@ki.se>
2013-11-05 15:47:39 +0100

    Wrap Javascript declarations inside an anonymous function

--- web/script.js
+++ web/script.js
@@ -1,39 +1,31 @@
-var resultRenderer
-var run
-var print
-
-function displayResult(json)
-{
-	var result = JSON.parse(json)
-	resultRenderer.render(result)
-}
-
-
-function submit(event)
-{
-	var n = document.getElementById("population-size").value
-	run(n)
-
+window.onload = function () {
+	var run = Module.cwrap('simulation_run', 'number', ['number'])
+	var print = Module.cwrap('simulation_print', 'number', ['number'])
+	var resultRenderer = Tempo.prepare("result")
 	var defaultLogger = window.console.log
-	window.console.log = displayResult
-	var asJson = 1
-	print(asJson)
-	window.console.log = defaultLogger
+
+	function displayResult(json)
+	{
+		var result = JSON.parse(json)
+		resultRenderer.render(result)
+	}
+
+
+	function submit(event)
+	{
+		var asJson = 1
+		
+		var n = document.getElementById("population-size").value		
+		run(n)
+
+		window.console.log = displayResult
+		print(asJson)
+		window.console.log = defaultLogger
 	
-	event.preventDefault()
-}
-
-
-function init()
-{
-	//initialize global variables
-	resultRenderer = Tempo.prepare("result")
-	run = Module.cwrap('simulation_run', 'number', ['number'])
-	print = Module.cwrap('simulation_print', 'number', ['number'])
+		event.preventDefault()
+	}
 
 	//register event handlers
 	var form = document.getElementById("input")
 	form.addEventListener("submit", submit, false)
 }
-
-window.onload = init
