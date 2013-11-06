7c71e3aecd40d42f1ef8be40f64dbd67dd5182e1
Karl Landström <karl.landstrom@ki.se>
2013-11-06 15:05:21 +0100

    Validate form input

--- web/script.js
+++ web/script.js
@@ -1,10 +1,12 @@
 window.onload = function () {
 	var run = Module.cwrap('simulation_run', 'number', ['number'])
 	var print = Module.cwrap('simulation_print', 'number', ['number'])
-	var resultRenderer = Tempo.prepare("result")
-	var execTimeRenderer = Tempo.prepare("execution-time")
-	var defaultLogger = window.console.log
 
+	var resultRenderer = Tempo.prepare("result-table")
+	var execTimeRenderer = Tempo.prepare("execution-time")
+	var errorRenderer = Tempo.prepare("error")
+
+	var defaultLogger = window.console.log
 	var executionTime //in seconds
 
 	function displayResult(json)
@@ -12,6 +14,7 @@ window.onload = function () {
 		var result = JSON.parse(json)
 		resultRenderer.render(result)
 		execTimeRenderer.render([[executionTime]])
+		document.getElementById("output").style.display = "block"
 	}
 
 
@@ -19,19 +22,29 @@ window.onload = function () {
 	{
 		var asJson = 1
 		
+		//clear previous output		
+		document.getElementById("output").style.display = "none"
+		errorRenderer.clear()
+		
 		var n = document.getElementById("population-size").value
-		var start = new Date()	
-		run(n)
-		executionTime = (new Date() - start) / 1000;
+		if (n.match(/^[0-9]+$/)) {
+			var start = new Date()	
+			run(n)
+			executionTime = (new Date() - start) / 1000;
 
-		window.console.log = displayResult
-		print(asJson)
-		window.console.log = defaultLogger
-	
+			window.console.log = displayResult
+			print(asJson)
+			window.console.log = defaultLogger
+		} else {
+			errorRenderer.render([[n]])
+		}	
 		event.preventDefault()
 	}
 
 	//register event handlers
 	var form = document.getElementById("input")
 	form.addEventListener("submit", submit, false)
+	
+	//hide output template
+	document.getElementById("output").style.display = "none"
 }
